f7bc69eac21854eeb9f46b351f9964f90cd31a80
greemoid <greemoiddd@gmail.com>
2023-02-22 10:42:04 +0200

    some changes

--- app/src/main/java/com/greemoid/breakthehabit/data/Result.kt
+++
@@ -1,6 +0,0 @@
-package com.greemoid.breakthehabit.data
-
-sealed class Result<out T : Any> {
-    data class Success<out T : Any>(val data: T) : Result<T>()
-    data class Failure(val exception: Exception) : Result<Nothing>()
-}
